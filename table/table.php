af3a283c89a1fff141bb31f3a2ba4d22921a0706
vaishnavikarakoti <160752764+vaishnavikarakoti@users.noreply.github.com>
2024-02-21 20:21:03 +0530

    Add files via upload

---
+++ table/table.php
@@ -0,0 +1,9 @@
+<html>
+<form action="tableCatch.php">
+Enter value 1:<input type="text" name="val1" />
+<br>
+Enter value 2:<input type="text" name="val2" />
+<br>
+<input type="submit" name="submit">
+</form>
+</hmtl>
