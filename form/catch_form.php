27bd3de6c1ca54cc1fd722444c1ba8721cc91936
vaishnavikarakoti <160752764+vaishnavikarakoti@users.noreply.github.com>
2024-02-21 20:19:26 +0530

    Add files via upload

---
+++ form/catch_form.php
@@ -0,0 +1,34 @@
+<?php
+$username=$_REQUEST['usernm'];
+$password=$_REQUEST['pwd'];
+$name=$_REQUEST['nm'];
+$addres=$_REQUEST['add'];
+$country=$_REQUEST['Country'];
+$zip_code=$_REQUEST['cod'];
+$email=$_REQUEST['email'];
+$sex=$_REQUEST['Sex'];
+$language=$_REQUEST['langu'];;
+$abt=$_REQUEST['detail'];
+
+echo "$username";
+echo "<br><br>";
+echo "$password";
+echo "<br><br>";
+echo "$name";
+echo "<br><br>";
+echo "$addres";
+echo "<br><br>";
+echo "$country";
+echo "<br><br>";
+echo "$zip_code";
+echo "<br><br>";
+echo "$email";
+echo "<br><br>";
+echo "$sex";
+echo "<br><br>";
+echo "$language";
+echo "<br><br>";
+echo "$abt";
+echo "<br><br>";
+echo "<table> </table>";
+?>
